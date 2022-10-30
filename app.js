d9453ccc584cfcfc53da16b4057f99e344cc8f7c
joseluis leon <joseluisleon@MacBook-Air-de-joseluis.local>
2022-10-29 20:12:12 -0500

    archivo modificado BUSCADOR

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 
+const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+];
 
-console.log('Hola mundo')
+const getHeroById = (id) => {
+    return heroes.find( hero => hero.id === id )
+}
+
+console.log(getHeroById(2))
+
+
+const getHeroesByOwner = (owner) => {
+    return heroes.filter( hero => hero.owner === owner )
+}
+
+console.log(getHeroesByOwner('Marvel'))
